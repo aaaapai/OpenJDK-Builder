f09054ad73c745d6aaa7229478d49a39c949d731
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-03-20 17:37:08 +0800

    Update build_jdk.bash

--- scripts/build_jdk.bash
+++ scripts/build_jdk.bash
@@ -31,6 +31,8 @@ cd ${CURRENT_DIR}/openjdk
 
 bash ./configure \
       --with-version-pre="-ea" \
+      --with-vendor-name="OpenJDK" \
+      --with-extra-build-info="built with aaaapai/OpenJDK-Builder" \
       --with-version-opt="${GITHUB_ACTOR}-${GITHUB_SHA}" \
       --with-conf-name="${TARGET}" \
       --with-boot-jdk-jvmargs="-XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+UseNUMA" \
@@ -43,6 +45,7 @@ bash ./configure \
       --enable-option-checking=fatal \
       --enable-linktime-gc \
       --disable-warnings-as-errors \
+      --enable-ccache \
       --with-debug-level=${JDK_DEBUG_LEVEL} \
       --with-fontconfig-include="${DEPS_INCLUDE_DIR}" \
       --with-devkit="${TOOLCHAIN}" \
@@ -53,23 +56,31 @@ bash ./configure \
       --with-extra-ldflags="${LDFLAGS}" \
       --with-freetype-include="${FREETYPE_DIR}/include/freetype2" \
       --with-freetype-lib="${FREETYPE_DIR}/lib" \
-      --x-libraries="/usr/lib" \
+      --x-libraries="${DEPS_LIB_DIR}" \ # TODO
       --with-toolchain-type=clang \
+      --x-includes="${DEPS_INCLUDE_DIR}/X11" \
       OBJDUMP="${OBJDUMP}" \
       STRIP="${STRIP}" \
       NM="${NM}" \
       AR="${AR}" \
       OBJCOPY="${OBJCOPY}" \
-      --x-includes="${DEPS_INCLUDE_DIR}/X11" \
       CC="${CC}" \
       CXX="${CXX}" \
       LD="${LD}" \
+      CXXFILT="llvm-cxxfilt" \
+      BUILD_CC="/usr/bin/clang" \
+	  	BUILD_CXX="/usr/bin/clang++" \
+      BUILD_NM="/usr/bin/llvm-nm" \
+	  	BUILD_AR="/usr/bin/llvm-ar" \
+	  	BUILD_OBJCOPY="/usr/bin/llvm-objcopy" \
+	  	BUILD_STRIP="/usr/bin/llvm-strip" \
+	  	--with-jobs=6
 
 cd ./build
 make -j6 images JOBS=6 || \
 error_code=$?
 
-if [[ "$error_code" -ne 0 ]]; then
-  echo "Build failure, exited with code $error_code."
+if [[ "${error_code}" -ne 0 ]]; then
+  echo "Build failure, exited with code ${error_code}."
   exit 1
 fi
