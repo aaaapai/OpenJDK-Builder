7f89afcdcf5bc5d0d4aa282792c641587f5b0ad6
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-03-21 21:54:20 +0800

    Update install_deps.bash

--- scripts/install_deps.bash
+++ scripts/install_deps.bash
@@ -18,7 +18,7 @@ cd ${CURRENT_DIR}/freetype
 bash ./autogen.sh
 bash ./configure \
     --host=${TARGET} \
-    --prefix=${CURRENT_DIR}/freetype/build \
+    --prefix=${PWD}/freetype/build_${TARGET} \
     --without-zlib \
     --with-brotli=system \
     --with-bzip2=no \
