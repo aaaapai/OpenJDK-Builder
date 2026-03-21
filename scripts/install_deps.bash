73ae86ea68253847c8c1093882d089176f6dfe8b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-03-21 16:18:40 +0800

    Update install_deps.bash

--- scripts/install_deps.bash
+++ scripts/install_deps.bash
@@ -103,7 +103,10 @@ iconv_cmake_build () {
 
 cd ${CURRENT_DIR}/libiconv
 iconv_cmake_build
-cp ${CURRENT_DIR}/libiconv/${TARGET}/install/libiconv.a ${DEPS_LIB_DIR}
-cp ${CURRENT_DIR}/libiconv/${TARGET}/install/libcharset.a ${DEPS_LIB_DIR}
-cp ${CURRENT_DIR}/libiconv/${TARGET}/install/include ${DEPS_INCLUDE_DIR}
-cp ${CURRENT_DIR}/libiconv/${TARGET}/install/include ${DEPS_INCLUDE_DIR}
+
+# 方法1：使用 find 命令查找库文件
+find ${CURRENT_DIR}/libiconv/${TARGET}/install -name "libiconv.a" -exec cp {} ${DEPS_LIB_DIR} \;
+find ${CURRENT_DIR}/libiconv/${TARGET}/install -name "libcharset.a" -exec cp {} ${DEPS_LIB_DIR} \;
+
+# 复制头文件（使用 -r 递归复制）
+cp -r ${CURRENT_DIR}/libiconv/${TARGET}/install/include ${DEPS_INCLUDE_DIR}
