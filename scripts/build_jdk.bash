817dd8753d61348866285edfe74c8bafbcca215e
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-03-21 19:49:38 +0800

    Update build_jdk.bash

--- scripts/build_jdk.bash
+++ scripts/build_jdk.bash
@@ -42,8 +42,8 @@ bash ./configure \
       --with-debug-level=${JDK_DEBUG_LEVEL} \
       --with-fontconfig-include="${DEPS_INCLUDE_DIR}" \
       --with-devkit="${NDK_TOOLCHAIN}" \
-	  --with-sysroot="${NDK_TOOLCHAIN}/sysroot"
-	  --with-toolchain-path="${NDK_TOOLCHAIN}/bin"
+	  --with-sysroot="${NDK_TOOLCHAIN}/sysroot" \
+	  --with-toolchain-path="${NDK_TOOLCHAIN}/bin" \
       --with-debug-level=${JDK_DEBUG_LEVEL} \
       --with-cups-include="${CUPS_DIR}" \
       --with-extra-cflags="${CFLAGS}" \
