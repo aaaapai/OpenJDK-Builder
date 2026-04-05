77f3faf54d0eadbc09cb0c2163aebadea1af39b0
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-05 17:56:51 +0800

    Update build_jdk.bash

--- scripts/build_jdk.bash
+++ scripts/build_jdk.bash
@@ -39,7 +39,7 @@ bash ./configure \
       --enable-linktime-gc \
       --disable-warnings-as-errors \
       --enable-ccache \
-	  -enable-cross-compile \
+	  --enable-cross-compile \
       --with-debug-level=${JDK_DEBUG_LEVEL} \
       --with-fontconfig-include="${DEPS_INCLUDE_DIR}" \
       --with-devkit="${NDK_TOOLCHAIN}" \
