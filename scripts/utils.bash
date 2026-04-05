12a8566a856949e3957de5259cceeba1d83d4a96
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-05 23:22:49 +0800

    Update utils.bash

--- scripts/utils.bash
+++ scripts/utils.bash
@@ -203,6 +203,14 @@ Set_LDFLAGS() {
     fi
 }
 
+get_compiler_with_ccache() {
+    local compiler="$1"
+    if command -v ccache >/dev/null 2>&1; then
+        echo "ccache ${compiler}"
+    else
+        echo "${compiler}"
+    fi
+}
 
 PrintConfigurationInfo() {
   echo "Current configuration:"
