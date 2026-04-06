80d2e99093c2177457577967ab14fdbfb63fd959
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-06 22:59:44 +0800

    Update build_jdk.bash

--- scripts/build_jdk.bash
+++ scripts/build_jdk.bash
@@ -61,8 +61,8 @@ bash ./configure \
       OBJCOPY="${OBJCOPY}" \
       CC="${CC}" \
       CXX="${CXX}" \
-	  BUILD_CC="${CC}" \
-	  BUILD_CXX="${CXX}" \
+	  BUILD_CC="/usr/bin/clang" \
+	  BUILD_CXX="/usr/bin/clang++" \
       LD="${LD}" \
 	  BUILD_LD="${LD}" \
       CXXFILT="llvm-cxxfilt" \
