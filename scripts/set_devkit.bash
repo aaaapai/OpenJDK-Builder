0ab0efa214c252666349aaee03965a96953e8706
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-13 23:59:01 +0800

    Update set_devkit.bash

--- scripts/set_devkit.bash
+++ scripts/set_devkit.bash
@@ -52,9 +52,9 @@ case "${TARGET_OS}" in
         export NM=${NDK_TOOLCHAIN}/bin/llvm-nm
         export DLLTOOL=${NDK_TOOLCHAIN}/bin/llvm-dlltool
 
-        Set_CFLAGS -I${CUPS_DIR} -I${DEPS_INCLUDE_DIR} -Wno-unknown-warning-option
-        Set_CPPFLAGS -I${CUPS_DIR} -I${DEPS_INCLUDE_DIR} -Wno-unknown-warning-option
-        Set_LDFLAGS -L${DEPS_LIB_DIR} -L${NDK_TOOLCHAIN}/sysroot/usr/lib/${TARGET}/${ANDROID_API}
+        Set_CFLAGS -I${FREETYPE_DIR}/include/freetype2 -I${CUPS_DIR} -I${DEPS_INCLUDE_DIR} -Wno-unknown-warning-option
+        Set_CPPFLAGS -I${FREETYPE_DIR}/include/freetype2 -I${CUPS_DIR} -I${DEPS_INCLUDE_DIR} -Wno-unknown-warning-option
+        Set_LDFLAGS -L${FREETYPE_DIR}/lib -L${DEPS_LIB_DIR} -L${NDK_TOOLCHAIN}/sysroot/usr/lib/${TARGET}/${ANDROID_API}
         ;;
     
     *)
