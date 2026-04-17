467a01d2d28a07bf48359d986e0dfeedd69c4063
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-17 17:59:14 +0800

    Update pack_java.bash

--- scripts/pack_java.bash
+++ scripts/pack_java.bash
@@ -3,11 +3,11 @@
 echo"Packing java..."
 
 
-cd ${CURRENT_DIR}/openjdk/build/${TARGET}/jre
+cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/jre
 tar cJf ../jre27-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
 
-cd ${CURRENT_DIR}/openjdk/build/${TARGET}/jdk
+cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/jdk
 tar cJf ../jdk27-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
 
-cd ${CURRENT_DIR}/openjdk/build/${TARGET}/dizout
-tar cJf ../jdk27debuginfo-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
+cd ${CURRENT_DIR}/openjdk/build/${TARGET}/images/symbols
+tar cJf ../symbols27-${TARGET_OS}-${TARGET_ARCH}-${GITHUB_SHA}.tar.xz .
