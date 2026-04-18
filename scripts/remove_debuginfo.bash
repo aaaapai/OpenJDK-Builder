796ddc72ed460c1cbb3b71107f452e6f80d56d82
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-18 12:34:30 +0800

    Update remove_debuginfo.bash

--- scripts/remove_debuginfo.bash
+++ scripts/remove_debuginfo.bash
@@ -29,6 +29,7 @@ for dir in jdk jre; do
     cp -v ${DEPS_LIB_DIR}/libawt_xawt.so images/${dir}/lib/ || true # It's needed for caciocavallo.
     cp -v ${DEPS_LIB_DIR}/libnuma.so images/${dir}/lib/ || true # Android doesn't have NUMA, it's a shim, perhaps there is no need to add it?
     cp -rv ${CURRENT_DIR}/fonts_config/* images/${dir}/lib/ || true # It's needed for caciocavallo.
+    cp -v images/jdk/lib/jspawnhelper images/${dir}/lib/libjspawnhelper.so || true
 done
 
 
