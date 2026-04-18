292de51e9f96773e2dd52f3334a06d1e9579d17f
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-18 09:13:08 +0800

    Update set_devkit.bash

--- scripts/set_devkit.bash
+++ scripts/set_devkit.bash
@@ -16,7 +16,7 @@ else
 fi
 
 if [[ "${TARGET_ARCH}" == "arm64" ]]; then
-  Set_C_CPPFLAGS -march=armv8-a+simd+sve
+  Set_C_CPPFLAGS -march=armv8-a+simd+sve -mbranch-protection=standard -D__ARM_NEON__=1
 fi
 
 if [[ "${ANDROID_API}" -ge 32 ]]; then
@@ -24,7 +24,8 @@ if [[ "${ANDROID_API}" -ge 32 ]]; then
   Set_LDFLAGS -Wl,-plugin-opt=-emulated-tls=0
 fi # Real LTS support is started at Android 12L, I disabled emulated lts here for better performence.
 
-Set_C_CPPFLAGS -O3 -fomit-frame-pointer -fno-semantic-interposition -mllvm -hot-cold-split=true -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fvectorize -fslp-vectorize -pipe -integrated-as
+Set_C_CPPFLAGS -O3 -fomit-frame-pointer -fno-semantic-interposition -mllvm -hot-cold-split=true -fdata-sections -ffunction-sections -ftree-vectorize -fvectorize -fslp-vectorize -pipe -integrated-as -fno-unique-section-names -fno-strict-aliasing -fno-math-errno -fno-ident -mno-outline -fvisibility=hidden -fvisibility-inlines-hidden -D__ANDROID_UNAVAILABLE_SYMBOLS_ARE_WEAK__ -DHAVE_SYS_UIO_H
+Set_CPPFLAGS -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -stdlib=libc++
 Set_LDFLAGS -fuse-ld=lld -Wl,--gc-sections -Wl,-O3 -Wl,--sort-common -Wl,--as-needed
 #LTO
 Set_C_CPPFLAGS -flto
@@ -32,6 +33,7 @@ Set_LDFLAGS -flto -Wl,--lto-O3
 #polly
 Set_C_CPPFLAGS -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -mllvm -polly-parallel
 
+Set_C_CPPFLAGS -fexperimental-relative-c++-abi-vtables
 
 case "${TARGET_OS}" in
     "ios")
