54b23f95e7fdd7f1b952ee33a86534554a0b5ce7
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-18 08:44:35 +0800

    Update build_jdk.bash

--- scripts/build_jdk.bash
+++ scripts/build_jdk.bash
@@ -6,32 +6,7 @@ cp -R /usr/include/X11 ${DEPS_INCLUDE_DIR}
 cp -R /usr/include/fontconfig ${DEPS_INCLUDE_DIR}
 cp ${CURRENT_DIR}/devkit_info/devkit.info.${TARGET_ARCH} ${NDK_TOOLCHAIN}
 
-if [[ "${TARGET_ARCH}" == "arm32" ]]; then
-  Set_C_CPPFLAGS -marm
-else
-  if [[ "${TARGET_ARCH}" == "x86" ]]; then
-     Set_C_CPPFLAGS -mstackrealign
-  fi
-fi
-
-if [[ "${TARGET_ARCH}" == "arm64" ]]; then
-  Set_C_CPPFLAGS -march=armv8-a+simd+sve
-fi
-
-if [[ "${ANDROID_API}" -ge 32 ]]; then
-  Set_C_CPPFLAGS -fno-emulated-tls
-  Set_LDFLAGS -Wl,-plugin-opt=-emulated-tls=0
-fi # Real LTS support is started at Android 12L, I disabled emulated lts here for better performence.
-
-Set_C_CPPFLAGS -O3 -fomit-frame-pointer -fno-semantic-interposition -mllvm -hot-cold-split=true -fdata-sections -ffunction-sections -fmerge-all-constants -ftree-vectorize -fvectorize -fslp-vectorize -pipe -integrated-as
-Set_LDFLAGS -fuse-ld=lld -Wl,--gc-sections -Wl,-O3 -Wl,--sort-common -Wl,--as-needed
-#LTO
-Set_C_CPPFLAGS -flto
-Set_LDFLAGS -flto -Wl,--lto-O3
-#polly
-Set_C_CPPFLAGS -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-detect-keep-going -mllvm -polly-ast-use-context -mllvm -polly-parallel
 Set_C_CPPFLAGS -DLE_STANDALONE
-
 
 chmod +x ${CURRENT_DIR}/wrapper/clang/buildcc-wrapped-clang
 chmod +x ${CURRENT_DIR}/wrapper/clang/buildcxx-wrapped-clang++
