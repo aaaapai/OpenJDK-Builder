28b5856bfa93e2e0c51da8de0fc434e60c9cfbac
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-19 14:50:32 +0800

    fix up

--- scripts/remove_debuginfo.bash
+++ scripts/remove_debuginfo.bash
@@ -17,6 +17,7 @@ cp -v buildjdk/jdk/lib/jspawnhelper buildjdk/jdk/lib/libjspawnhelper.so || true
 export JLINK_STRIP_ARG="--strip-native-debug-symbols=exclude-debuginfo-files:objcopy=${OBJCOPY}"
 
 ./buildjdk/jdk/bin/jlink \
+-Djdk.lang.Process.launchMechanism=FORK \
 --module-path=images/jdk/jmods \
 --add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.net.http,java.prefs,java.rmi,java.scripting,java.se,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,java.xml.crypto,jdk.accessibility,jdk.charsets,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.dynalink,jdk.editpad,jdk.httpserver,jdk.jdwp.agent,jdk.jfr,jdk.localedata,jdk.management,jdk.management.agent,jdk.management.jfr,jdk.naming.dns,jdk.naming.rmi,jdk.net,jdk.nio.mapmode,jdk.sctp,jdk.security.auth,jdk.security.jgss,jdk.unsupported,jdk.xml.dom,jdk.zipfs,jdk.hotspot.agent,jdk.incubator.vector,jdk.attach,jdk.jartool,jdk.jcmd,jdk.jconsole,jdk.jdeps,jdk.jdi,jdk.jpackage,jdk.jlink,jdk.jshell,jdk.jstatd,jdk.javadoc,jdk.unsupported.desktop,java.smartcardio,jdk.internal.jvmstat,jdk.internal.ed,jdk.internal.le,jdk.internal.md,jdk.internal.opt,${EXTRA_JLINK_JMODS} \
 --output images/jre \
