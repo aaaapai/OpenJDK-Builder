a77260c3e689fed1260d7ae2932ea42a9b3066e3
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-20 09:54:37 +0800

    Update remove_debuginfo.bash

--- scripts/remove_debuginfo.bash
+++ scripts/remove_debuginfo.bash
@@ -50,7 +50,7 @@ git clone --depth 1 https://github.com/termux/termux-elf-cleaner || true
 cd termux-elf-cleaner
 mkdir build
 cd build
-cmake .. -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_LINKER="/usr/bin/ld.lld" -DCMAKE_SYSROOT= -DCMAKE_FIND_ROOT_PATH= -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=NEVER -DCMAKE_C_FLAGS="" -DCMAKE_CXX_FLAGS=""
+cmake .. -DCMAKE_SYSTEM_NAME=Linux
 make -j6
 cd ../..
 
