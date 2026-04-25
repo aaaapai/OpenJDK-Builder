728f3db14c955ad18e7baabfbaaacaffc6f1445c
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-25 23:14:24 +0800

    Update set_devkit.bash

--- scripts/set_devkit.bash
+++ scripts/set_devkit.bash
@@ -31,9 +31,10 @@ if [[ "${ANDROID_API}" -ge 32 ]]; then
   Set_LDFLAGS -Wl,-plugin-opt=-emulated-tls=0
 fi # Real LTS support is started at Android 12L, I disabled emulated lts here for better performence.
 
-if [[ "${TARGET_ARCH}" != "arm32" && "${ANDROID_API}" -ge 24 ]]; then
+if [[ "${TARGET_ARCH}" != "arm32" && "${TARGET_ARCH}" != "x86" ]] || [[ "${ANDROID_API}" -ge 24 ]]; then
   Set_C_CPPFLAGS -D_FILE_OFFSET_BITS=64
 fi
+
 
 Set_C_CPPFLAGS -O3
 # Set_CPPFLAGS -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -stdlib=libc++
