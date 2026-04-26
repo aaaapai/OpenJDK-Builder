4cf297ba6b119c19d121df2448dbdb607285f181
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-26 12:46:32 +0800

    Update set_devkit.bash

--- scripts/set_devkit.bash
+++ scripts/set_devkit.bash
@@ -84,7 +84,7 @@ case "${TARGET_OS}" in
 
         Set_CFLAGS -I${FREETYPE_DIR}/include/freetype2 -I${CUPS_DIR} -I${DEPS_INCLUDE_DIR} -Wno-unknown-warning-option
         Set_CPPFLAGS -I${FREETYPE_DIR}/include/freetype2 -I${CUPS_DIR} -I${DEPS_INCLUDE_DIR} -Wno-unknown-warning-option
-        Set_LDFLAGS -L${FREETYPE_DIR}/lib -L${DEPS_LIB_DIR} -L${NDK_TOOLCHAIN}/sysroot/usr/lib/${TARGET}/${ANDROID_API}
+        Set_LDFLAGS -Wl,--enable-new-dtags -L${FREETYPE_DIR}/lib -L${DEPS_LIB_DIR} -L${NDK_TOOLCHAIN}/sysroot/usr/lib/${TARGET}/${ANDROID_API}
         ;;
     
     *)
