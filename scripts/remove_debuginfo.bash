38960a14060d4c3219b340568699ff9cb99cbe53
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-07-12 14:17:31 +0800

    Update remove_debuginfo.bash

--- scripts/remove_debuginfo.bash
+++ scripts/remove_debuginfo.bash
@@ -6,11 +6,6 @@ echo "Removing debuginfo..."
 cd ${CURRENT_DIR}/openjdk/build/${TARGET}
 
 
-if [[ "${TARGET_ARCH}" == "arm64" ]] || [[ "${TARGET_ARCH}" == "x86_64" ]] || [[ "${TARGET_ARCH}" == "riscv64" ]]; then
-   echo "Building for ${TARGET_ARCH}, introducing JVMCI module"
-   export EXTRA_JLINK_JMODS=jdk.internal.vm.ci
-fi
-
 cp -v buildjdk/jdk/lib/jspawnhelper buildjdk/jdk/lib/libjspawnhelper.so || true
 
 # Produce the jre equivalent from the jdk (https://blog.adoptium.net/2021/10/jlink-to-produce-own-runtime/)
@@ -19,7 +14,7 @@ export JAVA_TOOL_OPTIONS="-Xms3G -Xmx3G -XX:+UseThreadPriorities -XX:MetaspaceSi
 
 ./buildjdk/jdk/bin/jlink \
 --module-path=images/jdk/jmods \
---add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.net.http,java.prefs,java.rmi,java.scripting,java.se,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,java.xml.crypto,jdk.accessibility,jdk.charsets,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.dynalink,jdk.editpad,jdk.httpserver,jdk.jdwp.agent,jdk.jfr,jdk.localedata,jdk.management,jdk.management.agent,jdk.management.jfr,jdk.naming.dns,jdk.naming.rmi,jdk.net,jdk.nio.mapmode,jdk.sctp,jdk.security.auth,jdk.security.jgss,jdk.unsupported,jdk.xml.dom,jdk.zipfs,jdk.hotspot.agent,jdk.incubator.vector,jdk.attach,jdk.jartool,jdk.jcmd,jdk.jconsole,jdk.jdeps,jdk.jdi,jdk.jpackage,jdk.jlink,jdk.jshell,jdk.jstatd,jdk.javadoc,jdk.unsupported.desktop,java.smartcardio,jdk.internal.jvmstat,jdk.internal.ed,jdk.internal.le,jdk.internal.md,jdk.internal.opt,${EXTRA_JLINK_JMODS} \
+--add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.net.http,java.prefs,java.rmi,java.scripting,java.se,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,java.xml.crypto,jdk.accessibility,jdk.charsets,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.dynalink,jdk.editpad,jdk.httpserver,jdk.jdwp.agent,jdk.jfr,jdk.localedata,jdk.management,jdk.management.agent,jdk.management.jfr,jdk.naming.dns,jdk.naming.rmi,jdk.net,jdk.nio.mapmode,jdk.sctp,jdk.security.auth,jdk.security.jgss,jdk.unsupported,jdk.xml.dom,jdk.zipfs,jdk.hotspot.agent,jdk.incubator.vector,jdk.attach,jdk.jartool,jdk.jcmd,jdk.jconsole,jdk.jdeps,jdk.jdi,jdk.jpackage,jdk.jlink,jdk.jshell,jdk.jstatd,jdk.javadoc,jdk.unsupported.desktop,java.smartcardio,jdk.internal.jvmstat,jdk.internal.ed,jdk.internal.le,jdk.internal.md,jdk.internal.opt \
 --output images/jre \
 ${JLINK_STRIP_ARG} \
 --no-man-pages \
